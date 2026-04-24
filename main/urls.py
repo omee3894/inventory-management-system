82c81de59f8a1d1fb2e9b40f858e97354c3bb929
omee3894 <ommpatra491@gmail.com>
2026-04-24 06:04:49 +0530

    fix: remove recursive urls include

--- main/urls.py
+++ main/urls.py
@@ -19,6 +19,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('invoice/<int:bill_id>/', views.invoice, name='invoice'),
     path('create-employee/', create_employee, name='create_employee'),
-    path('', include('main.urls')),# or your app name
-
+   
 ]
